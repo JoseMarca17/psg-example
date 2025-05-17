0a397ab8715c31ab2fbf61b0863a9d67e2c79382
JoseMarca17 <josemarca1374@gmail.com>
2025-05-17 12:19:23 -0400

    Triangulo_prints

---
+++ retos_sesion_04/ejercicio_01.py
@@ -0,0 +1,13 @@
+#En este ejercicio vamos a crear un triangulo rectangulo con puros prints
+#Utilizaremos el signo $ para hacer esto
+
+print('$')
+print('$$')
+print('$$$')
+print('$$$$')
+print('$$$$$')
+print('$$$$$$')
+print('$$$$$$$')
+print('$$$$$$$$')
+print('$$$$$$$$$')
+print('$$$$$$$$$$')
