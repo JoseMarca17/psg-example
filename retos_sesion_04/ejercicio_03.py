ac2e306638881eebb50b3a961a4612c3cc59c480
JoseMarca17 <josemarca1374@gmail.com>
2025-05-17 12:51:13 -0400

    Juego de mesa favorito

---
+++ retos_sesion_04/ejercicio_03.py
@@ -0,0 +1,31 @@
+print('🃏-----REGLAS DEL UNO-----🎲')
+
+print("🃏1. Objetivo del juego:")
+print('\t- Ser el primero en quedarse sin cartas para ganar la ronda')
+
+print("🃏2. Cómo se juega")
+print("\t- Cada jugador recibe 7 cartas.")
+print("\t- El resto se coloca en el mazo de robar y se revela la primera carta \n")
+print('\t poniendola en el centro de la mesa')
+
+print("🃏3. En tu turno:")
+print("\t- Juega una carta que coincida en color, número o símbolo.")
+print("\t- Si no puedes jugar, (ninguna carta coicide) roba una carta del mazo.\n")
+
+print("🃏4. Cartas especiales:")
+print("\t- +2: El siguiente jugador roba 2 cartas y pierde su turno.")
+print("\t- Reversa: Cambia la dirección del juego.")
+print("\t- Salta: El siguiente jugador pierde su turno.")
+print("\t- Comodín: Cambia el color actual.")
+print("\t- Comodín +4: Cambia el color y el siguiente jugador roba 4 cartas.\n")
+
+print("🃏5. Gritar UNO:")
+print("\t- Cuando te quede solo una carta, grita 'UNO'.")
+print("\t- Si no lo haces y otro jugador lo nota, deberás robar 2 cartas.\n")
+
+print("🃏6. Fin del juego:")
+print("\t- El primer jugador en quedarse sin cartas gana la ronda.")
+print("\t- Si juegan con limite de tiempo, el que suma menos puntos con sus cartas gana la ronda.\n")
+
+print("Después de cada ronda, el mazo se vuelve a llenar con cartas aleatorias.")
+print('\n\t 🃏🎲DIVIERTETE CON ESTE MARAVILLOSO JUEGO!!!🎲🃏\n')
