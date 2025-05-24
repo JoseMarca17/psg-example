d14f3cf9a88b21688ce5798ee1fb627fcfa1a3b0
JoseMarca17 <josemarca1374@gmail.com>
2025-05-24 09:50:50 -0400

    Temperatura Farenheit a Celcius

---
+++ retos_sesion_05/ejercicio_02.py
@@ -0,0 +1,9 @@
+temperatura_far_1 = 25
+temperatura_far_2 = 300
+temperatura_far_3 = 76
+tem_celsius_1 = (temperatura_far_1 - 32)*(5/9)
+tem_celsius_2 = (temperatura_far_2 - 32)*(5/9)
+tem_celsius_3 = (temperatura_far_3 - 32)*(5/9)
+print("🌡️ La temperatura",temperatura_far_1,"F en Celsius es de ", tem_celsius_1, "C")
+print("🌡️ La temperatura",temperatura_far_2,"F en Celsius es de ", tem_celsius_2, "C")
+print("🌡️ La temperatura",temperatura_far_3,"F en Celsius es de ", tem_celsius_3, "C")
