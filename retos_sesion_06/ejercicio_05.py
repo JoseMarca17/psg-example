dd1a8b83cda539b1114414df5ebd30f27ef77c7c
JoseMarca17 <josemarca1374@gmail.com>
2025-05-31 12:41:05 -0400

    Misma paridad

---
+++ retos_sesion_06/ejercicio_05.py
@@ -0,0 +1,11 @@
+
+numero1 = 44
+numero2 = 98
+misma_paridad_1 = (numero1 % 2) == (numero2 % 2)
+
+numero3 = 111
+numero4 = 333
+misma_paridad_2 = (numero3 % 2) == (numero4 % 2)
+
+print(numero1, " y", numero2 ," tienen la misma paridad:", misma_paridad_1)
+print(numero3, " y",  numero4 ," tienen la misma paridad:", misma_paridad_2)
