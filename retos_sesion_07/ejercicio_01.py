d3a971cc9b096467c3d23598bb5015fa8314cbd2
JoseMarca17 <josemarca1374@gmail.com>
2025-06-07 14:38:34 -0400

    Frase libro

---
+++ retos_sesion_07/ejercicio_01.py
@@ -0,0 +1,10 @@
+frase =  """
+        Aprendi que no se puede dar marcha atras, 
+        que la esencia de la vida
+        es ir hacia adelantes. 
+        La vida, en realidad, 
+        es una calle de sentido unico. 
+        
+        - 📝 Agatha Christie 
+"""
+print(frase)
