713c2ecf097ea09d36dff33d8a9a2a7736179fe5
JoseMarca17 <josemarca1374@gmail.com>
2025-06-14 14:30:06 -0400

    Punto medio

---
+++ retos_sesion_08/ejercicio_06.py
@@ -0,0 +1,9 @@
+punto_1 = (-20, -20)
+punto_2 = (40, 20)
+
+punto_medio_x = (punto_1[0] + punto_2[0]) / 2
+punto_medio_y = (punto_1[1] + punto_2[1]) / 2
+
+punto_medio = (punto_medio_x, punto_medio_y)
+
+print("Coordenadas del punto medio:", punto_medio)
