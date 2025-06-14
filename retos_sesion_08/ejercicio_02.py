1b21380634eb0f873c38ac4b9ee528d0e6d10ae3
JoseMarca17 <josemarca1374@gmail.com>
2025-06-14 14:28:48 -0400

    Indice de tupla de letras

---
+++ retos_sesion_08/ejercicio_02.py
@@ -0,0 +1,11 @@
+tupla_letras = ('a','b','c','d','e','f','g','h','i','j')
+
+print("Primer elemento:", tupla_letras[0])
+
+print("Último elemento:", tupla_letras[-1])
+
+print("Slice del índice 3 al 5:", tupla_letras[3:6])
+
+print("Slice del 5 al 9 con pasos de 3:", tupla_letras[5:10:3])
+
+print("Slice del 9 al 0 con pasos de -2:", tupla_letras[9:0:-2])
