58603e45eb7b86531a1df57ad13e66ad6dbc7e25
JoseMarca17 <josemarca1374@gmail.com>
2025-06-14 14:28:20 -0400

    Coordenadas tupla

---
+++ retos_sesion_08/ejercicio_01.py
@@ -0,0 +1,7 @@
+x = int(input("Ingresa el valor de x: "))
+y = int(input("Ingresa el valor de y: "))
+z = int(input("Ingresa el valor de z: "))
+
+coordenadas = (x, y, z)
+
+print("Las coordenadas son: ", coordenadas)
