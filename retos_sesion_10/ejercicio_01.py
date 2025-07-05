0a06a6c10cfbca944809ddb87cff98fda4490fba
JoseMarca17 <josemarca1374@gmail.com>
2025-07-05 14:41:40 -0400

    Elementos repetidos

---
+++ retos_sesion_10/ejercicio_01.py
@@ -0,0 +1,5 @@
+cadena = "📎📐📏✏️🖊️🖋️📎📌📏📇🗂️📁📌🗃️✏️📂🖇️"
+conjunto = set(cadena)
+# Volvemos a convertir en una cadena 
+cadena_sin_repeticiones = ' '.join(conjunto)
+print("Sin repetidos:", cadena_sin_repeticiones)
