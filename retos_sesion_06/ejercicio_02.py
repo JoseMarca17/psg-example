59adacd20ecda3fa6158b14bbd16256a610e8eb3
JoseMarca17 <josemarca1374@gmail.com>
2025-07-10 12:08:43 -0400

    Cambios de la sesion 06

--- retos_sesion_06/ejercicio_02.py
+++ retos_sesion_06/ejercicio_02.py
@@ -1,9 +1,21 @@
+print("|   A   |   B   |  XNOR |")
+print("-"*25)
+a = False
+b = False
+resultado = (a and b) or (not a and not b)
+print(f"| {a} | {b} | {resultado}  |")
+
+a = False
+b = True
+resultado = (a and b) or (not a and not b)
+print(f"| {a} | {b}  | {resultado} |")
+
 a = True
 b = False
 resultado = (a and b) or (not a and not b)
-print(resultado)  
+print(f"| {a}  | {b} | {resultado} |")
 
 a = True
 b = True
 resultado = (a and b) or (not a and not b)
-print(resultado)  
+print(f"| {a}  | {b}  | {resultado}  |")
