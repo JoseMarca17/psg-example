71f6d8c03a0eefb49968dab013a99943b0f86261
JoseMarca17 <josemarca1374@gmail.com>
2025-07-12 13:53:28 -0400

    Clase sesion 11

---
+++ sesion08.py
@@ -0,0 +1,20 @@
+print ("Tupla de enteros")
+enteros = (1,2,3,4,5,6)
+print (enteros)
+print (type(enteros))
+
+matrix = ((0,1,2), (3,4,5), (6,7,8))
+print(f"Valor 2: {matrix[0][2]}") 
+print(matrix[1][1])
+print(matrix[2][0])
+
+
+cadenas = (("a", "es", "el", "programar"), 
+            ("estoy", "apre", "ar"), 
+            ("ndiendo", "a"))
+
+mensaje = (cadenas[1][0].capitalize() + " " +  
+        cadenas[1][1] + cadenas[2][0] + " " +  
+        cadenas[2][1] + " " +  
+        cadenas[0][3])  
+print(mensaje)
